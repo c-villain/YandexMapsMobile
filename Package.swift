00cb2e6ef108f9159b45d20dbf598a29ebbdf9d3
Alexander Kraev <lexkraev@gmail.com>
2022-05-05 14:47:53 +0300

    Add files via upload

---
+++ Package.swift
@@ -0,0 +1,30 @@
+// swift-tools-version: 5.6
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "YandexMapsMobile",
+    platforms: [
+        .iOS(.v11),
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "YandexMapsMobile",
+            targets: ["YandexMapsMobile"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .binaryTarget(
+            name: "YandexMapsMobile",
+            url: "https://github.com/c-villain/YandexMapsMobile/releases/download/4.0.0/YandexMapsMobile.xcframework.zip",
+            checksum: "b71370e1ece938e0db5b9fa8c410588a5509cef3d56a8a925debe08681acb948"
+        ),
+    ]
+)

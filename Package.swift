1448273b6104bfab077368d2a8d138aa2527ffae
Alexander Kraev <lexkraev@gmail.com>
2022-11-07 20:02:53 +0300

    update for 4.2.2

--- Package.swift
+++ Package.swift
@@ -23,8 +23,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
             name: "YandexMapsMobile",
-            url: "https://github.com/c-villain/YandexMapsMobile/releases/download/4.2.1/YandexMapsMobile.xcframework.zip",
-            checksum: "39dfada35ae5e7df7ee5df6d5f489906bc093c8da60bc829c2361d0920062892"
+            url: "https://github.com/c-villain/YandexMapsMobile/releases/download/4.2.2/YandexMapsMobile.xcframework.zip",
+            checksum: "5f2bd884971ee00130f9096d27c8f3edc8e58ec7965761314ffeaf3156defc95"
         ),
     ]
 )

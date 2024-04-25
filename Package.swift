68458f439c6bf8b45ddea07e152fa920b50c48ac
Alexander Kraev <lexkraev@gmail.com>
2024-04-25 17:19:33 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -18,8 +18,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "YandexMapsMobile",
-            url: "https://github.com/c-villain/YandexMapsMobile/releases/download/4.5.2/YandexMapsMobile.xcframework.zip",
-            checksum: "a39080fd4a56dd6d5eff79a003654df79d28f54062d3d786a196dabbc1712539"
+            url: "https://github.com/c-villain/YandexMapsMobile/releases/download/4.6.0/YandexMapsMobile.xcframework.zip",
+            checksum: "4677ba3d53fc46d1fff13dd2364766fa676373f2312275d3d9788f83f4e19688"
         ),
     ]
 )

fe639cd01011cd5b33b2759634001f8ef7052a9b
Alexander Kraev <lexkraev@gmail.com>
2024-09-25 12:39:49 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "YandexMapsMobile",
     platforms: [
-        .iOS(.v12),
+        .iOS(.v13),
     ],
     products: [
         .library(
